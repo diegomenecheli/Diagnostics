8e8f486b57e79f20a1f94dd0ac1aa8a11f8d6666
Menecheli <diegomenecheli@hotmail.com>
2024-03-11 23:33:03 -0300

    Inicial commit

---
+++ Diagnostics/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Diagnostics
+//
+//  Created by Diego Menecheli on 11/03/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
