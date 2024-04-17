88680b1f7472c06009a5ee5dd23958b8c9ba9cda
diegomenecheli <diegomenecheli@hotmail.com>
2024-04-17 00:03:42 -0300

    informationCell

---
+++ Diagnostics/Ui/Components/InformationCell.swift
@@ -0,0 +1,76 @@
+//
+//  InformationCell.swift
+//  Diagnostics
+//
+//  Created by Diego Menecheli on 17/04/24.
+//
+
+import Foundation
+import SwiftUI
+import SwiftData
+
+struct InformationCell: View {
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    
+    let icon: String!
+    let title: String
+    let value: String!
+    let destination: AnyView!
+    
+    init(title: String, value: String? = "", icon: String? = "", destination: AnyView? = nil) {
+        self.title = title
+        self.value = value
+        self.icon = icon
+        self.destination = destination
+    }
+    
+    
+    var body: some View {
+        if let destination = destination {
+            NavigationLink(destination: destination) {
+                content
+            }
+        } else {
+            content
+        }
+    }
+    
+    private var content: some View {
+        HStack {
+            if let icon = icon {
+                Image(systemName: icon)
+            }
+            
+            Text(title)
+                .font(.subheadline)
+            
+            Spacer()
+            
+            if let value = value {
+                Text(value)
+                    .font(.subheadline)
+                    .padding(.trailing)
+            }
+        }
+    }
+    
+}
+
+struct TestView: View {
+    
+    var body: some View {
+        List{
+            InformationCell(title: "Wi-Fi")
+            InformationCell(title: "Wi-Fi", value: "Connected")
+            InformationCell(title: "Wi-Fi", value: "Connected", icon: "wifi")
+            InformationCell(title: "Wi-Fi", value: "Connected", icon: "wifi", destination: AnyView(ResultsCompletedView()))
+
+        }
+    }
+}
+
+struct RowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
