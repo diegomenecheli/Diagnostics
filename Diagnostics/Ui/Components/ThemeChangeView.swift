bc35bbd273ed81bc2019188aeb3126218e7bdd7d
diegomenecheli <diegomenecheli@hotmail.com>
2024-04-17 00:14:24 -0300

    add: settings view

---
+++ Diagnostics/Ui/Components/ThemeChangeView.swift
@@ -0,0 +1,8 @@
+//
+//  ThemeChangeView.swift
+//  Diagnostics
+//
+//  Created by Diego Menecheli on 17/04/24.
+//
+
+import Foundation
---
+++ Diagnostics/Ui/View/SettingsView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsView.swift
+//  Diagnostics
+//
+//  Created by Diego Menecheli on 17/04/24.
+//
+
+import Foundation
